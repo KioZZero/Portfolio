2ad742677240790ef47a38b19438b8562cb5cf96
Niels JC. Diab <niels.diab@gmail.com>
2026-07-28 21:28:38 +0200

    [UPT] reset
    Signed-off-by: Niels JC. Diab <niels.diab@gmail.com>

--- Script/introOverlay.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const overlay = document.getElementById('intro-overlay')
-  if (overlay) {
-    console.log('ok');
-    overlay.addEventListener('animationend', () => overlay.remove())
-  }
-})
